dfb0448b99f0767f77b316464afe729700b0dc00
Maria <achigarpia@gmail>
2023-01-19 14:03:28 -0300

    Logo

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { showResult } from "./funciones/ShowResult";
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="./${viteLogo}" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
