1e64e47fa67462a80621bb7bdceba71cbb9adae7
Maria <achigarpia@gmail>
2023-01-19 12:40:21 -0300

    viteLogo

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import "./style.css";
 import javascriptLogo from "../src/fotos/javascript.svg";
+import viteLogo from "../src/fotos/vite.svg";
 import { setupCounter } from "../src/funciones/counter";
 import { showResult } from "./funciones/ShowResult";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
